fea07e4a9f026a78c84d002a6e48420d90436088
ZephirFX <zephirfx.pro@gmail.com>
2022-12-12 17:27:32 +0000

    Fix Typo

--- Lumen/Externals/tiny_obj_loader.h
+++ Lumen/Externals/tiny_obj_loader.h
@@ -1,26 +1,6 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2012-Present, Syoyo Fujita and many contributors.
-
-                             Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                                                          copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
-      */
+// Copyright (c) 2012-2022.
+// Enzo Crema
+// All rights reserved
 
 //
 // version 2.0.0 : Add new object oriented API. 1.x API is still provided.
@@ -1445,7 +1425,7 @@ inline real_t GetLength(TinyObjPoint &e) { return std::sqrt(e.x * e.x + e.y * e.
 
 inline TinyObjPoint Normalize(TinyObjPoint e)
 {
-    real_t inv_length = 1.0 / GetLength(e);
+    real_t inv_length = 1.0F / GetLength(e);
     return TinyObjPoint(e.x * inv_length, e.y * inv_length, e.z * inv_length);
 }
 
